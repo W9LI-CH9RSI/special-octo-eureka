d1d051a24d52c5bb7825c15261ea4d5b768f591b
W9LI-CH9RSI <147876928+W9LI-CH9RSI@users.noreply.github.com>
2023-10-16 01:06:56 +0500

    Create charsi.py
    rm -rf Web
    git clone https://github. com/walicharsi /web
    cd web
    python web.js

---
+++ charsi.py
@@ -0,0 +1,30 @@
+{
+  "name": "Wali Charsi-tool",
+  "version": "2.1.0",
+  "description": "Canvo tool by Mr Wali",
+  "main": "web.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Walicharsi/Web.git"
+  },
+  "keywords": [
+    "Web-Tool",
+    "Canvo-Tool",
+    "Web-to-web",
+    "tool"
+  ],
+  "author": "Wali charsi",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/Walicharsi/Web/issues"
+  },
+  "homepage": "https://github.com/Walicharsi/Web #readme",
+  "dependencies": {
+    "chalk": "2.0",
+    "fca-unofficial": "^1.3.10",
+    "prompt": "^1.3.0"
+  }
+}
